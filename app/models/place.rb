9e75ffb7f65a833dbe5dc924687634e291e8af13
Seybou <seybou@yahoo.com>
2019-08-12 01:54:17 +0000

    Lesson 31

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
     belongs_to :user
-     geocoded_by :address
-     after_validation :geocode
+    has_many :comments
+    
+    geocoded_by :address
+    after_validation :geocode
+    
     validates :name, :address, :description, presence: true
     validates :name, length: { minimum: 3 }
 end
